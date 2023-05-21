cd2d7579765f13c2519b3377d1abc800fb06cbcd
Viktor Szépe <viktor@szepe.net>
2023-05-21 12:48:13 +0000

    Fix CS in index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,7 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+/** Tell WordPress to load the WordPress theme and output it. */
+define('WP_USE_THEMES', true);
 
-/** Loads the WordPress Environment and Template */
+/** Load the WordPress Environment and Template. */
 require __DIR__ . '/project/wp-blog-header.php';
