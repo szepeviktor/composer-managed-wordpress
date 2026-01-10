5a7adbd7833d74cac38b34c15172cfb46160b954
Viktor Szépe <viktor@szepe.net>
2026-01-10 17:25:08 +0100

    Fix get_home_path

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Fix get_home_path() https://core.trac.wordpress.org/ticket/52575
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/project/index.php';
+
 /** Tell WordPress to load the WordPress theme and output it. */
 define('WP_USE_THEMES', true);
 
